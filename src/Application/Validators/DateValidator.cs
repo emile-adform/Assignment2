34c151854ec9043e442eef5f3073b563941186eb
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-02-03 22:23:50 +0200

    made validator for date validation

---
+++ src/Application/Validators/DateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Validators
+{
+    public class DateValidator : AbstractValidator<DateTime>
+    {
+        public DateValidator()
+        {
+            RuleFor(date => date)
+                .NotEmpty()
+                .Must(BeNotAfter2014).WithMessage("The date cannot be after 2014/12/31");
+        }
+        private bool BeNotAfter2014(DateTime date)
+        {
+            return date.Year < 2014;
+        }
+    }
+}
