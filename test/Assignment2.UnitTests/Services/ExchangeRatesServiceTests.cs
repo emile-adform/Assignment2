0188caa2e175d810f72ce73eb172ffaa212dae5e
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-02-10 21:46:07 +0200

    started creating tests

---
+++ test/Assignment2.UnitTests/Services/ExchangeRatesServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Castle.Core.Configuration;
+using Domain.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.UnitTests.Services
+{
+    public class ExchangeRatesServiceTests
+    {
+        private readonly Mock<IExchangeRatesRepository> _mockRepository;
+        private readonly IConfiguration _configuration;
+        private readonly ExchangeRatesServiceTests _service;
+        public ExchangeRatesServiceTests()
+        {
+            _mockRepository = new Mock<IExchangeRatesRepository>();
+            _service = new ExchangeRatesService()
+        }
+
+    }
+}
