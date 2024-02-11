bc798b7ce5141f623250a37119db86758d5be5ed
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-02-11 12:54:39 +0200

    wrote test for validation with invalid date

--- test/Assignment2.UnitTests/Services/ExchangeRatesServiceTests.cs
+++ test/Assignment2.UnitTests/Services/ExchangeRatesServiceTests.cs
@@ -1,5 +1,9 @@
-﻿using Castle.Core.Configuration;
+﻿using Application.Services;
+using Application.Validators;
+using Castle.Core.Configuration;
+using Domain.Exceptions;
 using Domain.Interfaces;
+using FluentAssertions;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -12,12 +16,24 @@ namespace Assignment2.UnitTests.Services
     public class ExchangeRatesServiceTests
     {
         private readonly Mock<IExchangeRatesRepository> _mockRepository;
-        private readonly IConfiguration _configuration;
-        private readonly ExchangeRatesServiceTests _service;
+        private readonly Mock<IExchangeRatesClient> _mockClient;
+        private readonly DateValidator _validator;
+        private readonly ExchangeRatesService _service;
         public ExchangeRatesServiceTests()
         {
             _mockRepository = new Mock<IExchangeRatesRepository>();
-            _service = new ExchangeRatesService()
+            _mockClient = new Mock<IExchangeRatesClient>();
+            _validator = new DateValidator();
+            _service = new ExchangeRatesService(_mockClient.Object, _validator, _mockRepository.Object);
+        }
+        [Fact]
+        public void InvalidDate_throwsInvalidDateException()
+        {
+            DateTime invalidDate = new DateTime(2023, 02, 02);
+
+            Action act = () => _service.Invoking(s => s.GetCurrencyChanges(invalidDate))
+                         .Should().ThrowAsync<InvalidDateException>()
+                         .WithMessage("The date cannot be after 2014/12/31");
         }
 
     }
