e9d4b9e2f9552e2fb163a54586f84fe0803e7391
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-02-16 15:20:19 +0200

    configured program.cs

--- src/Assignment2.WebApi/Program.cs
+++ src/Assignment2.WebApi/Program.cs
@@ -3,6 +3,8 @@ using Application;
 using Assignment2.WebApi.Middlewares;
 using Assignment2.WebApi.Authentication;
 using Assignment2.WebApi;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddMediatR(cfg => 
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
 builder.Services.AddWebApi();
 builder.Services.AddInfrastructure(builder.Configuration);
