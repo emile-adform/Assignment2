8e176549b5a6dc24fb9f8526b4adf0cd74928b67
e.skrupskaite <e.skrupskaite@DESKTOP-OUK37GL>
2024-02-16 15:59:44 +0200

    added new controller for testing

---
+++ src/Assignment2.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using Application.ExchangeRates.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment2.WebApi.Controllers
+{
+    [ApiController]
+    [Route("exchanges")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _mediator.Send(new GetExchangeRatesQuery()));
+        }
+    }
+}
